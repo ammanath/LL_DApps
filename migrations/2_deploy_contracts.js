18e894fd4e22b5c11bb91e4c35c3328e61ccf53d
akbrijesh <akbrijesh@gmail.com>
2021-09-20 10:04:05 +0530

    HelloWorld

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 var ApprovalContract = artifacts.require("ApprovalContract");
 var SimpleStorageContract = artifacts.require("SimpleStorage");
+var HelloWorldContract = artifacts.require("HelloWorld");
 
-module.exports = function(deployer){
+module.exports = function (deployer) {
     deployer.deploy(ApprovalContract);
     deployer.deploy(SimpleStorageContract);
+    deployer.deploy(HelloWorldContract);
 }
